a58635f9638fa6b78bd3b79249b203bb850ab200
Brady McDermott <freedom7341@hotmail.com>
2024-09-25 13:37:52 -0600

    boilerplate my beloved

---
+++ src/resource.h
@@ -0,0 +1,47 @@
+/* * * * * * * *\
+	RESOURCE.H -
+		Copyright © 2024 Brady McDermott
+	DESCRIPTION -
+		Configuration Utility's resource collection.
+		** NO DEPENDENCIES **
+	LICENSE INFORMATION -
+		MIT License, see LICENSE.txt in the root folder
+\* * * * * * * */
+
+/* Pragmas */
+#ifndef RC_INVOKED
+#pragma once
+#endif
+
+/* Icon Library */
+#define IDI_MAIN		1
+
+/* Bitmaps */
+#define IDB_MAX		1
+#define IDB_MAX_S	2
+#define IDB_MIN		3
+#define IDB_MIN_S	4
+#define IDB_RES		5
+#define IDB_RES_S	6
+#define IDB_MENU	7
+
+/* Dialogs */
+#define IDD_MAIN 101
+
+/* Dialog Controls */
+// Buttons
+#define IDC_STOP        40000
+#define IDC_START       40001
+#define IDC_CLOSE	    40002
+// Checkboxes
+#define IDC_THEMEOFF    40003
+// Dropdowns
+#define IDC_THEMES      40004
+
+/* String Table */
+#define IDS_APPTITLE	1
+#define IDS_WEBSITE		5
+// Dialog Theme Selection
+#define IDS_THEME1		200
+#define IDS_THEME2		201
+#define IDS_THEME3		202
